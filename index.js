82ad4a6ef79b09f2a061329001c87ac0b9bc19bc
KARONAGAMING <86901628+KARONAGAMING@users.noreply.github.com>
2021-12-01 14:00:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -205,7 +205,4 @@ process.on('multipleResolves', (type, promise, reason) => {
 
 //24/7 
 
-const express = require('express');
-const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
+
